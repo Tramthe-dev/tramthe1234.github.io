ee577f7796fa8bd4e305baa10cfd5d99a1ec69e5
tramthe1234.github.io <tramthe12345@gmail.com>
2023-10-30 16:57:52 +0700

    main

---
+++ content/post/vim.md
@@ -0,0 +1,15 @@
++++
+title = 'Vim'
+date = 2023-10-30T09:56:16Z
++++
+
+# How to install vim in termux
+
+```
+pkg update -y
+
+pkg install vim neovim -y
+
+```
+
+# Done
--- newpost.sh
+++ newpost.sh
@@ -1,6 +1,9 @@
 # wget https://raw.githubusercontent.com/tbotshub/tbotshub.github.io/main/.github/workflows/hugo.yml
 
-git add .
-git commit -m "deploy"
-git push -u origin main
+#git add .
+#git commit -m "deploy"
+#git push -u origin main
 
+git add .
+git commit -m main
+git push
